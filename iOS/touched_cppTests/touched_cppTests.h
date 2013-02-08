eb2996a9f8bf723966d39b1cfb90245e7b61eaf4
devmario <mario@vanillabreeze.com>
2013-02-08 18:15:53 +0900

    move json

---
+++ iOS/touched_cppTests/touched_cppTests.h
@@ -0,0 +1,13 @@
+//
+//  touched_cppTests.h
+//  touched_cppTests
+//
+//  Created by wonhee jang on 13. 2. 8..
+//  Copyright (c) 2013년 vanillabreeze. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface touched_cppTests : SenTestCase
+
+@end
---
+++ iOS/touched_cppTests/touched_cppTests.m
@@ -0,0 +1,32 @@
+//
+//  touched_cppTests.m
+//  touched_cppTests
+//
+//  Created by wonhee jang on 13. 2. 8..
+//  Copyright (c) 2013년 vanillabreeze. All rights reserved.
+//
+
+#import "touched_cppTests.h"
+
+@implementation touched_cppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in touched_cppTests");
+}
+
+@end
